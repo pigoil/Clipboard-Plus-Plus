bd07803e95f8256a230de3c212576f927e28361b
pigoil <alienanimal@163.com>
2017-08-04 16:30:50 +0800

    。

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QMessageBox::warning(0,"hahaha","sadsad");
 }
 
 MainWindow::~MainWindow()
